4df015a78a3153e31a36f6a7fb043037eeed73f1
Robert Smol <robert.smol@stereoit.com>
2013-02-28 00:00:38 +0100

    Initial comit

---
+++ src/kerowebsite/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.views.generic.base import TemplateView
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'kerowebsite.views.home', name='home'),
+    # url(r'^kerowebsite/', include('kerowebsite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    ('^$', TemplateView.as_view(template_name='index.html')),
+)
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                'document_root': settings.MEDIA_ROOT,
+        }),
+    )
+
